00115d98f77502fd90d169781fa4913aea6b186d
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2025-10-03 14:33:30 -0300

    fix: arrumei a função displayCart
    melhorei o display das informações

--- src/services/cart.js
+++ src/services/cart.js
@@ -3,9 +3,9 @@ async function addItem(userCart, item) {
 }
 
 async function deleteItem(userCart, index) {
-    const deleteIndex = index-1
-        console.log(deleteIndex)
-    if(index >= 0 && index < userCart.length){
+    const deleteIndex = index - 1
+    console.log(deleteIndex)
+    if (index >= 0 && index < userCart.length) {
         userCart.splice(deleteIndex, 1)
     }
 
@@ -14,7 +14,7 @@ async function deleteItem(userCart, index) {
 async function decreaseItemQuantity(userCart, item) {
     const indexItem = userCart.findIndex(p => p.name === item.name);
 
-    if(indexItem === -1){
+    if (indexItem === -1) {
         console.log("Item não encontrado")
         return
     }
@@ -24,16 +24,16 @@ async function decreaseItemQuantity(userCart, item) {
         return
     }
 
-    if(userCart[indexItem].quantity === 1){
+    if (userCart[indexItem].quantity === 1) {
         userCart.splice(indexItem, 1)
-    }  
+    }
 }
 
 async function calculateTotal(userCart) {
-  console.log("\nShopee Cart TOTAL IS:");
+    console.log("\nShopee Cart TOTAL IS:");
 
-  const result = userCart.reduce((total, item) => total + item.subTotal(), 0);
-  console.log(`Total: ${result}`);
+    const result = userCart.reduce((total, item) => total + item.subTotal(), 0);
+    console.log(`Total: ${result}`);
 }
 
 async function displayCart(userCart) {
@@ -41,6 +41,10 @@ async function displayCart(userCart) {
     userCart.forEach((item, index) => {
         console.log(`${index + 1}. ${item.name} - ${item.price} | x:${item.quantity} | Subtotal ${item.subTotal()}`)
     });
+    console.log("\nShopee Cart TOTAL IS:");
+
+    const result = userCart.reduce((total, item) => total + item.subTotal(), 0);
+    console.log(`Total: ${result}`);
 }
 
 export { addItem, deleteItem, decreaseItemQuantity, calculateTotal, displayCart };
